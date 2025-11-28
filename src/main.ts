4618040e58fab87dd6214e69f39b6beebc3baea9
alejandro23-10 <alexadron2310@gmail.com>
2025-11-28 19:29:22 +0300

    todocolumnDONE

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,47 @@ const reviewcolumn = document.getElementById("REVIEW")
 const donecolumn = document.getElementById("DONE")
 const addbutton = document.getElementById("ADDBUTTON")
 function handleaddbutton(){
+  var taskElement = document.createElement("div");
+  var inputElement = document.createElement("input");
+  var selectElement = document.createElement("select");
+  var optionTodo = document.createElement("option")
+  var optionInProgress = document.createElement("option")
+  var optionReview = document.createElement("option")
+  var optionDone = document.createElement("option")
+  var optionTodo = selectElement.appendChild(optionTodo) 
+  var optionInProgress = selectElement.appendChild(optionInProgress) 
+  var optionReview = selectElement.appendChild(optionReview) 
+  var optionDone = selectElement.appendChild(optionDone)
+  var selectElement = taskElement.appendChild(selectElement)
+  var inputElement = taskElement.appendChild(inputElement)
+  var taskElement = todocolumn?.appendChild(taskElement)
+  optionTodo.value = "todoColumn"
+  optionTodo.textContent = "todoColumn"
+  optionInProgress.value = "inprogresscolumn"
+  optionInProgress.textContent = "inprogresscolumn"
+  optionReview.value = "reviewcolumn"
+  optionReview.textContent = "reviewcolumn"
+  optionDone.value = "donecolumn"
+  optionDone.textContent = "donecolumn"
   console.log("hello world")
+  function handleSelect(event){
+    if(event.target.value == "inprogresscolumn"){
+      taskElement = inprogresscolumn?.appendChild(taskElement)
+      } 
+    if(event.target.value == "reviewcolumn"){
+      taskElement = reviewcolumn?.appendChild(taskElement)
+    }
+    if(event.target.value == "donecolumn"){
+      taskElement = donecolumn?.appendChild(taskElement)
+    }
+    if(event.target.value == "todoColumn"){
+      taskElement = todocolumn?.appendChild(taskElement)
+      }
+    }
+    console.log(event)
+  
+  selectElement.addEventListener("change", handleSelect)
+  
 };
 
 addbutton?.addEventListener("click", handleaddbutton)
